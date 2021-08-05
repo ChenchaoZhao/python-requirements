9a40d2fb165f50018cff03a4be85e1fb5da60343
Chenchao Zhao <chenchaozhao@Chenchaos-Air.lan>
2021-08-05 00:19:04 -0400

    add test

---
+++ test_research.py
@@ -0,0 +1,12 @@
+import numpy
+import scipy
+import pandas
+import torch
+import torchvision
+
+if __name__ == '__main__':
+    print(numpy.__version__)
+    print(scipy.__version__)
+    print(pandas.__version__)
+    print(torch.__version__)
+    print(torchvision.__version__)
